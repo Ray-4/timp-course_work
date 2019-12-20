34ee26fed6477a174b9faa4a3071e61d8dd74ded
Ray-4 <50871368+Ray-4@users.noreply.github.com>
2019-12-21 06:10:39 +0700

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -4,20 +4,16 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
-
-# Профиль создается при регистрации пользователя
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-# Профиль сохраняется при создании или редактировании.
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-# Файл изображения профиля удаляется при удалении профиля.
 @receiver(post_delete, sender=Profile)
 def auto_delete_profile_image_file_on_profile_delete(sender, instance, **kwargs):
     if instance.image:
@@ -25,8 +21,6 @@ def auto_delete_profile_image_file_on_profile_delete(sender, instance, **kwargs)
             os.remove(instance.image.path)
 
 
-"""
-# Файл изображения профиля удаляется при изменении изображения.
 @receiver(pre_save, sender=Profile)
 def auto_delete_profile_image_file_on_image_update(sender, instance, **kwargs):
     try:
@@ -38,4 +32,3 @@ def auto_delete_profile_image_file_on_image_update(sender, instance, **kwargs):
     if new_file != old_file:
         if os.path.isfile(old_file.path) and not old_file.path.endswith("/media/default.jpg"):
             os.remove(old_file.path)
-"""
