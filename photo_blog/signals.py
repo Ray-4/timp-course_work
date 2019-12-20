a8dcfbe4f3faea98ca1a7f237e365df5f5499977
Ray-4 <50871368+Ray-4@users.noreply.github.com>
2019-12-21 05:56:26 +0700

    Update signals.py

--- photo_blog/signals.py
+++ photo_blog/signals.py
@@ -4,8 +4,6 @@ from django.dispatch import receiver
 from .models import Post, Comment, Notification
 from users.models import Profile
 
-
-# Файл изображения удаляется при удалении сообщения.
 @receiver(post_delete, sender=Post)
 def auto_delete_file_on_post_delete(sender, instance, **kwargs):
     if instance.photo:
@@ -13,15 +11,13 @@ def auto_delete_file_on_post_delete(sender, instance, **kwargs):
             os.remove(instance.photo.path)
 
 
-# Создает объект уведомления, если комментарий к сообщению сделан не автором,
-# а другим пользователем.
 @receiver(post_save, sender=Comment)
 def auto_create_comment_notification(sender, instance, created, **kwargs):
     if created:
         if instance.author != instance.post.author:
             Notification.objects.create(post=instance.post, comment=instance, date_posted=instance.date_posted)
 
-# Создает экземпляр уведомления, если пост понравился.
+
 @receiver(m2m_changed, sender=Post.likes.through)
 def auto_create_like_notification(sender, instance, action, pk_set, **kwargs):
     if action == "post_add":
@@ -32,7 +28,6 @@ def auto_create_like_notification(sender, instance, action, pk_set, **kwargs):
         Notification.objects.filter(user_id=pk, post=instance).delete()
 
 
-# Создает экземпляр уведомления, если за пользователем следят.
 @receiver(m2m_changed, sender=Profile.followers.through)
 def auto_create_follow_notification(sender, instance, action, pk_set, **kwargs):
     if action == "post_add":
